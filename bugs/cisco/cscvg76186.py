94e7c367b32d848d14c023edc8f942be2caa3afd
Dom932 <38622219+Dom932@users.noreply.github.com>
2018-05-22 22:40:40 +0100

    Add documentation to the class

--- bugs/cisco/cscvg76186.py
+++ bugs/cisco/cscvg76186.py
@@ -3,7 +3,32 @@ from bugs.base_bug import BaseBug
 
 class CSCvg76186(BaseBug):
     """
-    TODO
+    Cisco Smart Install is a feature in Cisco IOS and OIS XE software that provides
+    plug and play configuration and image management for zero touch deployment of new
+    equipment.
+
+    A vulnerablility in the Smar Instll freature could allow an attacker to trigger a
+    reload of a device or execute arbitrary code. This attack is possible due to
+    improper validation of packet data. In order to exploit this vulnerablity, an
+    attacker would create a Smart Install message to an vulnerable device on port
+    tcp/4786, which could cause a buffer overflow.
+
+    This bug check works by checking the output of "show vstack config" to determin
+    if the Smart Install feature is enabled (which it is by default). Note this bug
+    check does not currently check IOS version comparison and the output is only to
+    indicate if a device might be affected (ie a upgraded IOS version (with the bug
+    fix) with Smart Install enable would be marked as affected, Please check the IOS
+    version).
+
+    Cisco Bug ID: CSCvg76186
+    Advisory ID: CVE-2018-0171
+    CVSS Score: 9.8 (Critical)
+    Reference: https://tools.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-20180328-smi2
+
+    Notes:
+        Please review and understand the code before running it on any device.
+        This bug check is provided "as is" and is to be used at your own risk.
+
     """
 
     @property
@@ -17,7 +42,7 @@ class CSCvg76186(BaseBug):
     @property
     def requirements(self):
         """
-        Requirements for check_bug - connection only required
+        Requirements for check_bug
         :return:
         """
         return ["connection"]
