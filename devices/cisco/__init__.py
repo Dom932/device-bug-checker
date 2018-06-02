5087b940011436019e9405d1e18ef3587f9e63b5
Dom932 <38622219+Dom932@users.noreply.github.com>
2018-06-02 23:28:04 +0100

    Implemented Cisco IOS Telnet

--- devices/cisco/__init__.py
+++ devices/cisco/__init__.py
@@ -1,4 +1,5 @@
 from devices.cisco.cisco_ios import CiscoIOS
+from devices.cisco.cisco_ios_telnet import CiscoIOSTelnet
 from devices.cisco.cisco_wlc import CiscoWLC
 from devices.cisco.cisco_asa import CiscoASA
 from devices.cisco.cisco_nxos import CiscoNXOS
---
+++ devices/cisco/cisco_ios_telnet.py
@@ -0,0 +1,20 @@
+import re
+from devices.cisco import CiscoIOS
+
+class CiscoIOSTelnet(CiscoIOS):
+    """
+    Class to represent Cisco IOS device
+    """
+
+    def __init__(self, **kwargs):
+        super(CiscoIOSTelnet, self).__init__(**kwargs)
+
+
+    @property
+    def device_type(self):
+        """
+        Returns device type
+        :param self:
+        :return list:
+        """
+        return 'cisco_ios_telnet'
